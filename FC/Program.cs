e2cccb111cb6dda9c9289ad640dc6686ea489b0a
Ben <s.i.ares@mail.ru>
2022-12-09 13:06:06 +0300

    add Editing text and solution

--- FC/Program.cs
+++ FC/Program.cs
@@ -1,1 +1,31 @@
 ﻿
+
+string[] array = new string[] { "qwerty", "123", "Vi", "Ru", ": )", "asdfg" };
+
+PrintArray(array);
+Console.WriteLine();
+SOrt(array);
+Console.Write("Final: ");
+PrintArray(array); 
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+void SOrt(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        string element = arr[i];
+        if (element.Length <= 3)
+        {
+            arr[i] = element;
+    }
+        else arr[i] = string.Empty;
+    }
+} 
+
